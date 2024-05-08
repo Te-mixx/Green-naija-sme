922e2b5c8952d215b11194ab83875d682bdf83c0
Te-mixx <temisann@gmail.com>
2024-05-08 18:25:36 +0100

    update config details to be fetched from Config class

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,23 +1,17 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskr.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
-app.config['MAIL_SERVER'] = 'wghp5.wghservers.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('PASSWORD')
 mail = Mail(app)
 
 
