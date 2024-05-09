983e69e077db0fe8dcc7782c7fe4400e4e4db6a7
Te-mixx <temisann@gmail.com>
2024-05-09 17:15:40 +0100

    fix linting with flake8

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,31 +1,43 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import (DataRequired, Length,
+                                Email, EqualTo, ValidationError)
 from flaskr.models import User
 
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    company_name = StringField('Company Name', validators=[DataRequired(), Length(min=3, max=50)])
-    company_description = StringField('Company Description', validators=[DataRequired(), Length(min=10, max=300)])
+    company_name = StringField('Company Name',
+                               validators=[DataRequired(),
+                                           Length(min=3, max=50)])
+    company_description = StringField('Company Description',
+                                      validators=[DataRequired(),
+                                                  Length(min=10, max=300)])
     address = StringField('Address', validators=[DataRequired()])
     city = StringField('City')
     state = StringField('State', validators=[DataRequired()])
     zip_code = StringField('Zip')
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
     submit = SubmitField('Sign-up')
 
-    def validate_email(self, email): 
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError("Email is already taken, try another email address!")
-        
+            raise ValidationError("Email is already taken, \
+                                  try another email address!")
+
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError("Username is already taken, try another username!")
+            raise ValidationError("Username is already taken, \
+                                  try another username!")
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -33,8 +45,10 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Update')
 
@@ -42,24 +56,31 @@ class UpdateAccountForm(FlaskForm):
         if email.data != current_user.username:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError("Email is already taken, try another email address!")
-        
+                raise ValidationError("Email is already taken, \
+                                      try another email address!")
+
     def validate_username(self, username):
         if username.data != current_user.email:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError("Username is already taken, try another username!")
+                raise ValidationError("Username is already taken, \
+                                      try another username!")
+
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request password reset')
 
-    def validate_email(self, email): 
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
-            raise ValidationError("There is no account with that email, kindly create an account.")
+            raise ValidationError("There is no account with that email, \
+                                  kindly create an account.")
+
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
     submit = SubmitField('Reet password')
